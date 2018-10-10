4ed4dec7366371907e2ec48a2e4544de7ff66e68
Max Ramos <mramos@virtela.net>
2018-10-10 18:30:13 +0800

    Updated ws config.

--- src/main/java/com/maxaramos/springwstest/WsConfig.java
+++ src/main/java/com/maxaramos/springwstest/WsConfig.java
@@ -2,12 +2,14 @@ package com.maxaramos.springwstest;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 
-//@Configuration
+@Configuration
 public class WsConfig {
 
 //	@Bean
@@ -18,17 +20,17 @@ public class WsConfig {
 		return new ServletRegistrationBean<>(messageDispatcherServlet, "/services/*");
 	}
 
-//	@Bean(name = "users")
-	public DefaultWsdl11Definition userWsdl() {
+	@Bean(name = "users")
+	public DefaultWsdl11Definition usersWsdl() {
 		DefaultWsdl11Definition wsdl = new DefaultWsdl11Definition();
 		wsdl.setPortTypeName("UserService");
 //		wsdl.setLocationUri("/services/");
-		wsdl.setSchema(userSchema());
+		wsdl.setSchema(userXsd());
 		return wsdl;
 	}
 
-//	@Bean
-	public SimpleXsdSchema userSchema() {
+	@Bean
+	public SimpleXsdSchema userXsd() {
 		return new SimpleXsdSchema(new ClassPathResource("/wsdl/user.xsd"));
 	}
 
